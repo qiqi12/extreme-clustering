c0fad1159cb3341f8518289d69695ac4d8663083
liqi bit <2399256801@qq.com>
2021-11-08 10:45:40 +0800

    Delete Python version directory

--- Python version/main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from  Extreme_Clustering import Extreme_Clustreing
-from  Visualization import Visualization
-
-
-data = np.loadtxt("data-sets/s1.txt")
-clusteringResult = Extreme_Clustreing(data, 70000, False)
-Visualization(data, clusteringResult, False)
